c56baddfc2e7982161661c8e83a4cd0ffb486fd1
IcyretszMacbookPro16 <ubw1212@gmail.com>
2024-11-24 20:29:58 +0700

    Setting up

--- app/features/auth/components/user.tsx
+++ app/features/auth/components/user.tsx
@@ -3,13 +3,14 @@ import {useUser} from '@auth0/nextjs-auth0/client';
 
 const User = () => {
 
-  const {user, isLoading, error} = useUser();
+  const {user} = useUser();
 
   return (
     <>
       {user && (
         <div>
           <p>Hello, {user?.name}</p>
+          {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
           <a href="/api/auth/logout">Logout</a>
         </div>
       )}
